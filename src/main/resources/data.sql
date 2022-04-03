3c856a016cec665e92ae9977b85038125568920b
bstasz <stasz.balazs@gmail.com>
2022-04-03 21:25:08 +0200

    Added PostgreSQL connection + data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM person;
+
+INSERT INTO person (id, first_name, last_name) VALUES (1, 'John', 'Doe');
+INSERT INTO person (id, first_name, last_name) VALUES (2, 'Bill', 'Gates');
+INSERT INTO person (id, first_name, last_name) VALUES (3, 'Ano', 'Nymous');
+INSERT INTO person (id, first_name, last_name) VALUES (4, 'Max', 'Min');
