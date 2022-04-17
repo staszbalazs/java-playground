95c79dde403d97fd1693e6111476b36c8b1bc29a
bstasz <stasz.balazs@gmail.com>
2022-04-17 14:45:24 +0200

    Added Flyway

--- src/main/resources/db/migration/V0_0_1__example.sql
+++ src/main/resources/db/migration/V0_0_1__example.sql
@@ -1,4 +1,8 @@
-DELETE FROM person;
+CREATE TABLE person (
+    id bigint PRIMARY KEY,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL
+);
 
 INSERT INTO person (id, first_name, last_name) VALUES (1, 'John', 'Doe');
 INSERT INTO person (id, first_name, last_name) VALUES (2, 'Bill', 'Gates');
